5e1dcaddf967541c74158039e41d3236535d9dbd
Stefano <stefano@shellrent.com>
2015-04-18 08:19:00 +0200

    [strong mod]

---
+++ src/Sircamp/Xenapi/Element/XenElement.php
@@ -0,0 +1,41 @@
+<?php namespace Sircamp\Xenapi\Element;
+
+use Respect\Validation\Validator as Validator;
+use GuzzleHttp\Client as Client;
+use Sircamp\Xenapi\Connection\XenConnection as  XenConnection;
+
+
+class XenElement {
+	
+	private $xenconnection;
+
+
+	function __construct($xenconnection){
+		$this->xenconnection = $xenconnection;
+	}
+
+    /**
+     * Gets the value of xenconnection.
+     *
+     * @return mixed
+     */
+    public function getXenconnection()
+    {
+        return $this->xenconnection;
+    }
+
+    /**
+     * Sets the value of xenconnection.
+     *
+     * @param mixed $xenconnection the xenconnection
+     *
+     * @return self
+     */
+    private function _setXenconnection($xenconnection)
+    {
+        $this->xenconnection = $xenconnection;
+
+        return $this;
+    }
+}
+?>
